a772d2c913e3c7d8a9e3bd894b11fe998b38023f
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-09-22 12:35:41 +0300

    move test to dedicated repo
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- generate.go
+++
@@ -1,3 +0,0 @@
-package grpc
-
-//go:generate protoc -I./proto -I. --go-grpc_out=paths=source_relative:./proto --go_out=paths=source_relative:./proto --micro_out=paths=source_relative:./proto proto/test.proto
